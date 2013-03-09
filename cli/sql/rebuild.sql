5aef7ad3c91286dab2bc17a8aa85a7ab4953242f
Mark Dexter <dextercowley@gmail.com>
2013-03-08 21:10:44 -0800

    Add rebuild queries

---
+++ cli/sql/rebuild.sql
@@ -0,0 +1,63 @@
+# type 1 created issues
+INSERT INTO jos_code_activity_detail (activity_type, activity_xref_id, jc_user_id, jc_issue_id, activity_date)
+SELECT 1, i.jc_issue_id, u.jc_user_id, i.jc_issue_id, i.created_date
+FROM jos_code_tracker_issues AS i
+JOIN jos_code_users AS u
+ON i.created_by = u.user_id
+WHERE tracker_id = 3;
+
+# type 2 - comments
+INSERT INTO jos_code_activity_detail (activity_type, activity_xref_id, jc_user_id, jc_issue_id, activity_date)
+SELECT 2, i.response_id, u.jc_user_id, i.jc_issue_id, i.created_date
+FROM jos_code_tracker_issue_responses AS i
+JOIN jos_code_users AS u
+ON i.created_by = u.user_id
+WHERE tracker_id = 3;
+
+# type 3 Insert status change activity
+INSERT INTO jos_code_activity_detail (activity_type, activity_xref_id, jc_user_id, jc_issue_id, activity_date)
+SELECT 3, i.change_id, u.jc_user_id, i.jc_issue_id, i.change_date
+FROM jos_code_tracker_issue_changes AS i
+JOIN jos_code_users AS u
+ON i.change_by = u.user_id
+WHERE tracker_id = 3;
+
+# 4 - tests done
+INSERT INTO jos_code_activity_detail (activity_type, activity_xref_id, jc_user_id, jc_issue_id, activity_date)
+SELECT 4, i.response_id, u.jc_user_id, i.jc_issue_id, i.created_date
+FROM jos_code_tracker_issue_responses AS i
+JOIN jos_code_users AS u
+ON i.created_by = u.user_id
+WHERE tracker_id = 3
+AND i.body LIKE '%@test%';
+
+# type 5 Patch files
+INSERT INTO jos_code_activity_detail (activity_type, activity_xref_id, jc_user_id, jc_issue_id, activity_date)
+SELECT 5, i.file_id, u.jc_user_id, i.jc_issue_id, i.created_date
+FROM jos_code_tracker_issue_files AS i
+JOIN jos_code_users AS u
+ON i.created_by = u.user_id
+WHERE tracker_id = 3
+AND i.name LIKE '%patch%' OR i.name LIKE '%diff%';
+
+# type 6 Pull requests in a comment
+INSERT INTO jos_code_activity_detail (activity_type, activity_xref_id, jc_user_id, jc_issue_id, activity_date)
+SELECT 6, i.response_id, u.jc_user_id, i.jc_issue_id, i.created_date
+FROM jos_code_tracker_issue_responses AS i
+JOIN jos_code_users AS u
+ON i.created_by = u.user_id
+WHERE tracker_id = 3
+AND i.body LIKE '%/pull%';
+
+# type 7 Pull requests in original bug report
+INSERT INTO jos_code_activity_detail (activity_type, activity_xref_id, jc_user_id, jc_issue_id, activity_date)
+SELECT 7, i.jc_issue_id, u.jc_user_id, i.jc_issue_id, i.created_date
+FROM jos_code_tracker_issues AS i
+JOIN jos_code_users AS u
+ON i.created_by = u.user_id
+WHERE tracker_id = 3
+AND description LIKE '%/pull%';
+
+SELECT activity_type AS type, count(*)
+FROM jos_code_activity_detail
+GROUP BY activity_type
