0ca3a801df4c4a93ca486641a83d277337ca64e4
Mark Dexter <dextercowley@gmail.com>
2013-03-13 11:40:33 -0700

    Back-end skeleton files for trackerstats

---
+++ public_html/administrator/components/com_trackerstats/trackerstats.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @version		$Id: code.php 398 2010-06-13 17:53:03Z louis $
+ * @package		Joomla.Administrator
+ * @subpackage	com_code
+ * @copyright	Copyright (C) 2009 - 2010 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ * @since		1.6
+ */
+
+defined('_JEXEC') or die;
+?>
+<h1>Tracker statistics </h1>
