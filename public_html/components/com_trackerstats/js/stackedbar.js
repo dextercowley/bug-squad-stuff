a254b50a4fd0df620ab5a2ffbff14137311940bb
Mark Dexter <dextercowley@gmail.com>
2013-03-15 22:57:58 -0700

    Stack horizontal bar chart with jqplot!

--- public_html/components/com_trackerstats/js/stackedbar.js
+++ public_html/components/com_trackerstats/js/stackedbar.js
@@ -7,11 +7,12 @@ jQuery(document).ready(function($){
     var ticks = ['May', 'June', 'July', 'August'];
      
     var plot1 = $.jqplot('chart1', [s1, s2, s3], {
+    	stackSeries: true,
         // The "seriesDefaults" option is an options object that will
         // be applied to all series in the chart.
         seriesDefaults:{
             renderer:$.jqplot.BarRenderer,
-            rendererOptions: {fillToZero: true}
+            rendererOptions: {fillToZero: true, barDirection: 'horizontal'}
         },
         // Custom labels for the series are specified with the "label"
         // option on the series option.  Here a series option object
@@ -31,16 +32,16 @@ jQuery(document).ready(function($){
         },
         axes: {
             // Use a category axis on the x axis and use our custom ticks.
-            xaxis: {
+            yaxis: {
                 renderer: $.jqplot.CategoryAxisRenderer,
                 ticks: ticks
             },
             // Pad the y axis just a little so bars can get close to, but
             // not touch, the grid boundaries.  1.2 is the default padding.
-            yaxis: {
-                pad: 1.05,
-                tickOptions: {formatString: '$%d'}
-            }
+//            yaxis: {
+//                pad: 1.05,
+//                tickOptions: {formatString: '$%d'}
+//            }
         }
     });
 });
