ac32c6db366350a1dba09129813962a26796d98b
Mark Dexter <dextercowley@gmail.com>
2013-03-16 07:45:51 -0700

    partial AJAX for stacked bar. numbers coming from ajax, need to figure out how to pass ticks and labels

--- public_html/components/com_trackerstats/js/ajaxrenderer.js
+++ public_html/components/com_trackerstats/js/ajaxrenderer.js
@@ -19,15 +19,52 @@ jQuery(document).ready(function($){
  
   // The url for our json data
   var jsonurl = $("#chart2").attr("href");
+  var ticks = ['May', 'June', 'July', 'August'];
  
   // passing in the url string as the jqPlot data argument is a handy
   // shortcut for our renderer.  You could also have used the
   // "dataRendererOptions" option to pass in the url.
-  var plot2 = $.jqplot('chart2', jsonurl,{
-    title: "AJAX JSON Data Renderer",
-    dataRenderer: ajaxDataRenderer,
+  var plot2 = $.jqplot('chart2', jsonurl, {
+  	title: "AJAX JSON Data Renderer",
+  	dataRenderer: ajaxDataRenderer,
     dataRendererOptions: {
       unusedOptionalUrl: jsonurl
-    }
+    },
+  	stackSeries: true,
+      // The "seriesDefaults" option is an options object that will
+      // be applied to all series in the chart.
+      seriesDefaults:{
+          renderer:$.jqplot.BarRenderer,
+          rendererOptions: {fillToZero: true, barDirection: 'horizontal'}
+      },
+      // Custom labels for the series are specified with the "label"
+      // option on the series option.  Here a series option object
+      // is specified for each series.
+      series:[
+          {label:'Hotel'},
+          {label:'Event Regristration'},
+          {label:'Airfare'}
+      ],
+      // Show the legend and put it outside the grid, but inside the
+      // plot container, shrinking the grid to accomodate the legend.
+      // A value of "outside" would not shrink the grid and allow
+      // the legend to overflow the container.
+      legend: {
+          show: true,
+          placement: 'outsideGrid'
+      },
+      axes: {
+          // Use a category axis on the x axis and use our custom ticks.
+          yaxis: {
+              renderer: $.jqplot.CategoryAxisRenderer,
+              ticks: ticks
+          },
+          // Pad the y axis just a little so bars can get close to, but
+          // not touch, the grid boundaries.  1.2 is the default padding.
+//          yaxis: {
+//              pad: 1.05,
+//              tickOptions: {formatString: '$%d'}
+//          }
+      }
   });
 });
--- public_html/components/com_trackerstats/views/dashboard/tmpl/getchart2data.php
+++ public_html/components/com_trackerstats/views/dashboard/tmpl/getchart2data.php
@@ -2,5 +2,5 @@
 $s1 = array(200,600,700,1000);
 $s2 = array(460, 210, 690, 820);
 $s3 = array(260, 440, 320, 200);
-echo json_encode(array($s1));
+echo json_encode(array($s1,$s2,$s3));
 die();
