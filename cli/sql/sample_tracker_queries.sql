cfae7d46c1a7132d4973a01eb8069bcb77a410ba
Mark Dexter <dextercowley@gmail.com>
2013-03-21 09:23:38 -0700

    Lanaguage tags for back end

--- cli/sql/sample_tracker_queries.sql
+++ cli/sql/sample_tracker_queries.sql
@@ -1,3 +1,15 @@
+# Release Notes
+SELECT CASE WHEN ISNULL(m.tag) THEN 'None' ELSE m.tag END as Category, 
+i.title as 'Issue Title', i.jc_issue_id, i.close_date
+FROM `jos_code_tracker_issues` i
+LEFT JOIN `jos_code_tracker_issue_tag_map` m 
+ON i.issue_id = m.issue_id
+AND m.tag_id IN (39,1,29,44,36,85,11,40,17,82,13,6,35,22,27,21,23,20,49,34,19,25,43,94,88)
+WHERE DATE(close_date) BETWEEN '2011-01-10' AND '2011-02-22'
+AND status_name IN('Fixed in SVN')
+AND i.tracker_id = 3
+ORDER BY CASE WHEN ISNULL(m.tag) THEN 'None' ELSE m.tag END, close_date asc
+
 # Closed by Month
 SELECT COUNT(*) AS total_closed, 
 SUM(
