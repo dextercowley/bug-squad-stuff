1e65b3debfd224233706ed74f96dd02ec4ab51ee
Mark Dexter <dextercowley@gmail.com>
2013-03-31 18:20:50 -0700

    Use #__code_users instead of #__users.

--- public_html/modules/mod_trackerstats_users/helper.php
+++ public_html/modules/mod_trackerstats_users/helper.php
@@ -18,14 +18,14 @@ abstract class modTrackerstatsUsersHelper
 
 		$db		= JFactory::getDbo();
 		$query	= $db->getQuery(true);
-		$query->select('u.name, SUM(t.activity_points) AS total_points');
+		$query->select('CONCAT(u.first_name, " ", u.last_name) AS name, SUM(t.activity_points) AS total_points');
 		$query->from('#__code_activity_detail AS a');
 		$query->join('INNER', '#__code_activity_types AS t ON a.activity_type = t.activity_type');
-		$query->join('INNER', '#__users AS u ON a.user_id = u.id');
+		$query->join('INNER', '#__code_users AS u ON a.jc_user_id = u.jc_user_id');
 		$query->where('date(a.activity_date) > Date(DATE_ADD(now(), INTERVAL -' . $days . ' DAY))');
-		$query->group('u.id, u.name');
+		$query->group('u.jc_user_id, CONCAT(u.first_name, " ", u.last_name)');
 		$query->having('SUM(t.activity_points) > ' . (int) $points);
-		$query->order('u.name ASC');
+		$query->order('CONCAT(u.first_name, " ", u.last_name) ASC');
 
 		$db->setQuery($query);
 		$rows = (array) $db->loadObjectList();
