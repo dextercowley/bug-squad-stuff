d08b4a2d51c00c043060fed0cea4a2911888a95d
Mark Dexter <dextercowley@gmail.com>
2013-04-07 17:06:27 -0700

    Test queries for total issue open / close chart

--- cli/sql/sample_tracker_queries.sql
+++ cli/sql/sample_tracker_queries.sql
@@ -10,23 +10,70 @@ AND status_name LIKE '%Fixed in SVN%'
 AND i.tracker_id = 3
 ORDER BY CASE WHEN ISNULL(m.tag) THEN 'None' ELSE m.tag END, close_date asc
 
-# Closed by Month
+# Total Closed by Month
 SELECT COUNT(*) AS total_closed, 
 SUM(
-CASE WHEN DATE(i.close_date) BETWEEN Date(DATE_ADD(now(), INTERVAL -119 DAY)) AND Date(DATE_ADD(now(), INTERVAL -90 DAY))
+CASE WHEN (DATE(i.close_date) BETWEEN Date(DATE_ADD(now(), INTERVAL -119 DAY)) AND Date(DATE_ADD(now(), INTERVAL -90 DAY)))
+AND status_name like '%fixed%'
 THEN 1 ELSE 0 END
-) AS p4,
+) AS fixed_p4,
 SUM(
-CASE WHEN DATE(i.close_date) BETWEEN Date(DATE_ADD(now(), INTERVAL -89 DAY)) AND Date(DATE_ADD(now(), INTERVAL -60 DAY))
+CASE WHEN (DATE(i.close_date) BETWEEN Date(DATE_ADD(now(), INTERVAL -89 DAY)) AND Date(DATE_ADD(now(), INTERVAL -60 DAY)))
+AND status_name like '%fixed%'
 THEN 1 ELSE 0 END
-) AS p3,
+) AS fixed_p3,
 SUM(
-CASE WHEN DATE(i.close_date) BETWEEN Date(DATE_ADD(now(), INTERVAL -59 DAY)) AND Date(DATE_ADD(now(), INTERVAL -30 DAY))
+CASE WHEN (DATE(i.close_date) BETWEEN Date(DATE_ADD(now(), INTERVAL -59 DAY)) AND Date(DATE_ADD(now(), INTERVAL -30 DAY)))
+AND status_name like '%fixed%'
 THEN 1 ELSE 0 END
-) AS p2,
+) AS fixed_p2,
 SUM(
-CASE WHEN DATE(i.close_date) BETWEEN Date(DATE_ADD(now(), INTERVAL -29 DAY)) AND Date(DATE_ADD(now(), INTERVAL -0 DAY))
+CASE WHEN (DATE(i.close_date) BETWEEN Date(DATE_ADD(now(), INTERVAL -29 DAY)) AND Date(DATE_ADD(now(), INTERVAL -0 DAY)))
+AND status_name like '%fixed%'
 THEN 1 ELSE 0 END
-) AS p1
+) AS fixed_p1,
+SUM(
+CASE WHEN (DATE(i.close_date) BETWEEN Date(DATE_ADD(now(), INTERVAL -119 DAY)) AND Date(DATE_ADD(now(), INTERVAL -90 DAY)))
+AND status_name NOT like '%fixed%'
+THEN 1 ELSE 0 END
+) AS closed_p4,
+SUM(
+CASE WHEN (DATE(i.close_date) BETWEEN Date(DATE_ADD(now(), INTERVAL -89 DAY)) AND Date(DATE_ADD(now(), INTERVAL -60 DAY)))
+AND status_name NOT like '%fixed%'
+THEN 1 ELSE 0 END
+) AS closed_p3,
+SUM(
+CASE WHEN (DATE(i.close_date) BETWEEN Date(DATE_ADD(now(), INTERVAL -59 DAY)) AND Date(DATE_ADD(now(), INTERVAL -30 DAY)))
+AND status_name NOT like '%fixed%'
+THEN 1 ELSE 0 END
+) AS closed_p2,
+SUM(
+CASE WHEN (DATE(i.close_date) BETWEEN Date(DATE_ADD(now(), INTERVAL -29 DAY)) AND Date(DATE_ADD(now(), INTERVAL -0 DAY)))
+AND status_name NOT like '%fixed%'
+THEN 1 ELSE 0 END
+) AS closed_p1
 FROM `eem_code_tracker_issues` AS i
 WHERE DATE(i.close_date) BETWEEN Date(DATE_ADD(now(), INTERVAL -119 DAY)) AND Date(now())
+AND i.state = 0
+
+
+# Total open by Month
+SELECT COUNT(*) AS total_closed, 
+SUM(
+CASE WHEN DATE(i.created_date) BETWEEN Date(DATE_ADD(now(), INTERVAL -119 DAY)) AND Date(DATE_ADD(now(), INTERVAL -90 DAY))
+THEN 1 ELSE 0 END
+) AS total_opened_p4,
+SUM(
+CASE WHEN DATE(i.created_date) BETWEEN Date(DATE_ADD(now(), INTERVAL -89 DAY)) AND Date(DATE_ADD(now(), INTERVAL -60 DAY))
+THEN 1 ELSE 0 END
+) AS total_opened_p3,
+SUM(
+CASE WHEN DATE(i.created_date) BETWEEN Date(DATE_ADD(now(), INTERVAL -59 DAY)) AND Date(DATE_ADD(now(), INTERVAL -30 DAY))
+THEN 1 ELSE 0 END
+) AS total_opened_p2,
+SUM(
+CASE WHEN DATE(i.created_date) BETWEEN Date(DATE_ADD(now(), INTERVAL -29 DAY)) AND Date(DATE_ADD(now(), INTERVAL -0 DAY))
+THEN 1 ELSE 0 END
+) AS total_opened_p1
+FROM `eem_code_tracker_issues` AS i
+WHERE DATE(i.created_date) BETWEEN Date(DATE_ADD(now(), INTERVAL -119 DAY)) AND Date(now())
